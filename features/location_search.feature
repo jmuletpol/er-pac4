b9fbdbcb99e68b5c0db13aa8542b9da42e1adf70
Josep Mulet Pol <pep.mulet@gmail.com>
2023-01-08 11:52:40 +0100

    waze app

--- features/location_search.feature
+++ features/location_search.feature
@@ -1,5 +1,29 @@
-Feature: Cercador d'ubicacions
+Feature: Cercador d'ubicacions de l'app web Waze
 
-  Scenario: Test Cookbook Title Description
-    Given I visit Test Cookbook website
-    Then I see title Test Cookbook
+  Scenario: Detecta automàticament totes les localitats i llocs el nom dels quals coincideix amb el fragment introduït per l'usuari
+    Given Suposant que l'usuari visita "Waze" a "https://www.waze.com/ca/live-map"
+    And que no ha iniciat sessió amb cap usuari
+    And el mapa està centrat en la regió Espanya
+    When introdueix el text "cuenc" al camp ubicació d'origen
+    Then es desplega un llista d'opcions que conté "CuencaEspanya"
+
+  Scenario: Detecta ubicacions sense tenir en compte les majúscules i minúscules.
+    Given Suposant que l'usuari visita "Waze" a "https://www.waze.com/ca/live-map"
+    And que no ha iniciat sessió amb cap usuari
+    And el mapa està centrat en la regió Espanya
+    When introdueix el text "cuENca" al camp ubicació d'origen
+    Then es desplega un llista d'opcions que conté "CuencaEspanya"
+
+  Scenario: Detecta ubicacions sense tenir en compte els accents introduïts per error.
+    Given Suposant que l'usuari visita "Waze" a "https://www.waze.com/ca/live-map"
+    And que no ha iniciat sessió amb cap usuari
+    And el mapa està centrat en la regió Espanya
+    When introdueix el text "cuéncà" al camp ubicació d'origen
+    Then es desplega un llista d'opcions que conté "CuencaEspanya"
+
+  Scenario: Detecta ubicacions sense tenir en compte caràcters especials.
+    Given Suposant que l'usuari visita "Waze" a "https://www.waze.com/ca/live-map"
+    And que no ha iniciat sessió amb cap usuari
+    And el mapa està centrat en la regió Espanya
+    When introdueix el text "c'u'en'ca" al camp ubicació d'origen
+    Then es desplega un llista d'opcions que conté "CuencaEspanya"
--- features/step_definitions/location_search.steps.js
+++ features/step_definitions/location_search.steps.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const {Given, Then, When, Before, After} = require('@cucumber/cucumber');
-const { Builder } = require("selenium-webdriver")
+const { Builder, By } = require("selenium-webdriver")
 
 let driver;
 
@@ -11,15 +11,48 @@ Before(async () => {
 
 After(async() => {
   if(driver!=null) {
-    //await driver.quit();
+   await driver.quit();
   }
 })
-
-Given(/^I visit Test Cookbook website$/, async() => {
-    await driver.get('http://www.testcookbook.com');
+ 
+Given('Suposant que l\'usuari visita {string} a {string}', async function(title, url) {
+    await driver.get(url);
+    const currentTitle = await driver.getTitle();
+    assert(currentTitle.indexOf(title)>=0);
+}); 
+ 
+Given('que no ha iniciat sessió amb cap usuari', async() => {
+    const hasLoginBtn = await driver.findElement(By.className("wz-header__login-button")).isDisplayed();
+    assert(hasLoginBtn);
 });
 
-Then(/^I see title Test Cookbook$/, async () => {
-    const title = await driver.getTitle()
-    assert.equal(title, "Test Cookbook");    
+Given('el mapa està centrat en la regió Espanya', async() => {
+  await driver.get("https://www.waze.com/ca/live-map/directions?latlng=39.44871624877421%2C-5.799222886562347");
+  const inputDesti = await driver.findElement(By.css('div.is-origin input.wm-search__input'));
+  await driver.sleep(2000);
+  await inputDesti.clear();
 });
+
+When('introdueix el text {string} al camp ubicació d\'origen', async (valor) => {
+  const inputSortida = await driver.findElement(By.css('div.is-origin input.wm-search__input'));
+  await inputSortida.clear();
+  await inputSortida.sendKeys(valor);
+});
+
+Then('es desplega un llista d\'opcions que conté {string}', async(resultat) => {
+  await driver.sleep(1000);
+  const searchSuggestions = await driver.findElement(By.id("search-suggestions"));
+  const allResults = await searchSuggestions.findElements(By.css("li.wm-search-item"));
+  //console.log("Results found ", allResults.length)
+  let found = false;
+  let i = 0;
+  const len = allResults.length;
+  while(i<len && !found) { 
+    const value = await allResults[i].getText();
+    found = value === resultat;
+    i++;
+  }
+  assert(found);
+});
+
+  
